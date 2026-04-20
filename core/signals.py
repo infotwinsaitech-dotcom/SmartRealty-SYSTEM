60afd144274000f9d6410cb71b1f99754715a6de
Prince026 <pckavathiya1@gmail.com>
2026-04-21 01:41:56 +0530

    fix static

--- core/signals.py
+++ core/signals.py
@@ -1,15 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from .models import Agent, Builder  # अगर builder भी है
+from .models import Agent
 
 User = get_user_model()
 
 @receiver(post_save, sender=User)
-def create_profiles(sender, instance, created, **kwargs):
-    if created:
-        if instance.role == "agent":
-            Agent.objects.create(user=instance)
-
-        elif instance.role == "builder":
-            Builder.objects.create(user=instance)
+def create_agent_profile(sender, instance, created, **kwargs):
+    if created and instance.role == "agent":
+        Agent.objects.create(
+            user=instance,
+            name=instance.username,
+            email=instance.email,
+            phone=instance.phone or ""
+        )
