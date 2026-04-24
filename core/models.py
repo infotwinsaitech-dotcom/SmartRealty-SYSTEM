f52c1edc01864e78a1d4075f9bcdbda1e52ffa81
Prince026 <pckavathiya1@gmail.com>
2026-04-24 21:55:44 +0530

    add cloudinary dependency

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib import admin
-
+from cloudinary.models import CloudinaryField
 
 class Property(models.Model):
     title = models.CharField(max_length=255)
