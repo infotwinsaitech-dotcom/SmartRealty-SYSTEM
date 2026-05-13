d74dda6ad04f8e08ac232074c8b98abe8786dd67
Prince2202031800047 <pckavathiya1@gmail.com>
2026-05-13 23:17:13 +0530

    apply amenities migration

--- core/models.py
+++ core/models.py
@@ -111,7 +111,7 @@ class UserAdmin(admin.ModelAdmin):
 class Agent(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15, null=True, blank=True)
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
