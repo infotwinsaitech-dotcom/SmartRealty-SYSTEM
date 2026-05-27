3d10d07242bc343c42c5c92ebb1e13205dcdd216
Prince2202031800047 <pckavathiya1@gmail.com>
2026-05-27 14:32:33 +0530

    added template filters

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -2,16 +2,17 @@ from django import template
 
 register = template.Library()
 
-@register.filter
+@register.filter(name='format_price')
 def format_price(value):
     try:
         value = float(value)
 
         if value >= 10000000:
-            return f"{round(value/10000000, 2)} Cr"
+            return f"{value/10000000:.1f} Cr"
         elif value >= 100000:
-            return f"{round(value/100000, 2)} Lakh"
+            return f"{value/100000:.0f} Lakh"
         else:
-            return f"{int(value)}"
+            return value
+
     except:
         return value
