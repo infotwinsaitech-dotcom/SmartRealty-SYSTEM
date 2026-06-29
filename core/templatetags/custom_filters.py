f7d0c19ac259b81bf121c427a978587363e3e1eb
Prince2202031800047 <pckavathiya1@gmail.com>
2026-06-29 10:02:28 +0530

    fix home url redirect and email backend

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -16,3 +16,14 @@ def format_price(value):
 
     except:
         return value
+    
+from django import template
+
+register = template.Library()
+
+@register.filter
+def div(value, arg):
+    try:
+        return float(value) / float(arg)
+    except (ValueError, ZeroDivisionError, TypeError):
+        return 0
