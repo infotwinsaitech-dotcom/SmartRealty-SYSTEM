0489ac1463f2c4f4c0d7f611b5e04050e8283e63
Prince2202031800047 <pckavathiya1@gmail.com>
2026-07-01 16:15:20 +0530

    fix home url redirect and email backend

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -2,24 +2,20 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='format_price')
 def format_price(value):
     try:
         value = float(value)
-
         if value >= 10000000:
             return f"{value/10000000:.1f} Cr"
         elif value >= 100000:
             return f"{value/100000:.0f} Lakh"
         else:
-            return value
-
-    except:
+            return str(value)
+    except Exception:
         return value
-    
-from django import template
 
-register = template.Library()
 
 @register.filter
 def div(value, arg):
